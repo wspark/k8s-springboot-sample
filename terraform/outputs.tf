558920248192d7acd33d5b3520f62326604e0f2d
박원석 <jjomacson@gmail.com>
2022-09-18 15:08:05 +0900

    output.tf modify

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,24 @@
 output "cluster_id" {
   description = "EKS cluster ID"
-  value       = module.eks.cluster_id
+  value       = "education-eks-VDvcQfAm"
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+  value       = "https://F49377BCC88C7440EA2438E6B7227C4C.sk1.ap-northeast-2.eks.amazonaws.com"
 }
 
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+  value       = "sg-0c9513affa712229c"
 }
 
 output "region" {
   description = "AWS region"
-  value       = var.region
+  value       = "ap-northeast-2"
 }
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = local.cluster_name
+  value       = "education-eks-VDvcQfAm"
 }
